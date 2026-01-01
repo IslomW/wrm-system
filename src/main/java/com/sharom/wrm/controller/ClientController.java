41d9b21b7badd39a347063dcc059fdfee76b78f5
Islom <tawkenskiy000@gmail.com>
2026-01-01 13:15:29 +0500

    feat: ClientController

--- src/main/java/com/sharom/wrm/controller/ClientController.java
+++ src/main/java/com/sharom/wrm/controller/ClientController.java
@@ -1,4 +1,58 @@
 package com.sharom.wrm.controller;
 
+import com.sharom.wrm.payload.UserDTO;
+import com.sharom.wrm.service.ClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/clients")
+@RequiredArgsConstructor
 public class ClientController {
+    private final ClientService clientService;
+
+
+
+
+    // CREATE
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO) {
+        UserDTO createdUser = clientService.create(userDTO);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    // GET BY ID
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getById(@PathVariable String id) {
+        UserDTO user = clientService.getById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    // GET ALL
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAll() {
+        List<UserDTO> users = clientService.getAll();
+        return ResponseEntity.ok(users);
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> update(
+            @PathVariable String id,
+            @RequestBody UserDTO userDTO
+    ) {
+        UserDTO updatedUser = clientService.update(id, userDTO);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        clientService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
